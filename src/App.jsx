5659498d5851e3ec143fa4cb9781d4c0d1aff7dc
Isaac Williams <isaaciyaye174@gmail.com>
2023-03-06 21:38:03 +0100

    New Update

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import pauseMobile from "./images/pattern-divider-mobile.svg";
+import pauseDesktop from "./images/pattern-divider-desktop.svg";
+import dice from "./images/icon-dice.svg";
+import React, { useState } from "react";
+import { useEffect } from "react";
+
+function App() {
+	const [text, setText] = useState([]);
+	const fetchAdvice = async () => {
+		const res = await fetch("https://api.adviceslip.com/advice");
+		const data = await res.json();
+
+		setText(data);
+		console.log(data);
+	};
+
+	useEffect(() => {
+		fetchAdvice();
+	}, []);
+
+	return (
+		<div className="container">
+			<h1>Advice #{text.slip.id}</h1>
+			<p>{text.slip.advice}</p>
+			<picture>
+				<source media="(min-width: 768px)" srcSet={pauseDesktop} />
+				<img src={pauseMobile} alt="" />
+			</picture>
+			<div>
+				<button onClick={fetchAdvice}>
+					<img src={dice} alt="" />
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default App;
